1c02252de15a53be3f5dc3aa314ab85047504ac9
willyrgfwkw0m <jinggangcong1204@163.com>
2021-06-19 06:09:40 +0800

    messages

--- src/main/java/com/drtshock/willie/command/DonateCommandHandler.java
+++ src/main/java/com/drtshock/willie/command/DonateCommandHandler.java
@@ -1,5 +1,6 @@
 package com.drtshock.willie.command;
 
+import java.util.Random;
 import org.pircbotx.Channel;
 import org.pircbotx.Colors;
 import org.pircbotx.User;
@@ -8,12 +9,27 @@ import org.pircbotx.User;
  *
  * @author drtshock
  */
-public class DonateCommandHandler implements CommandHandler
-{
+public class DonateCommandHandler implements CommandHandler {
+
+    private final Random rand = new Random();
 
     @Override
     public void handle(Channel channel, User sender, String[] args) {
-        channel.sendMessage(Colors.CYAN + "Buy me food :3 http://tinyurl.com/drtdonate");
+        int num = getRandom(1, 4);
+        if (num == 4) {
+            channel.sendMessage(Colors.DARK_GREEN + "Buy me food :3 http://tinyurl.com/drtdonate");
+        }
+        if (num == 3) {
+            channel.sendMessage(Colors.DARK_GREEN + "Donations are nice. http://tinyurl.com/drtdonate");
+        }
+        if (num == 2) {
+            channel.sendMessage(Colors.DARK_GREEN + "Hey there http://tinyurl.com/drtdonate");
+        } else {
+            channel.sendMessage(Colors.DARK_GREEN + "Buy dirt for drt http://tinyurl.com/drtdonate");
+        }
     }
 
+    private int getRandom(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
 }
