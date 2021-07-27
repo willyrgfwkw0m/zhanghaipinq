5d07d0b7c13b0cdc919ebc8d0dc06b1c85849a36
willyrgfwkw0m <jinggangcong1204@163.com>
2021-07-27 15:45:20 +0800

    Reload and join commands.

--- src/main/java/com/drtshock/willie/Willie.java
+++ src/main/java/com/drtshock/willie/Willie.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import java.util.Timer;
 import java.util.logging.Logger;
 
+import com.drtshock.willie.command.*;
 import org.pircbotx.Base64;
 import org.pircbotx.Channel;
 import org.pircbotx.Colors;
@@ -17,17 +18,6 @@ import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
 import org.pircbotx.exception.NickAlreadyInUseException;
 
-import com.drtshock.willie.command.CICommandHandler;
-import com.drtshock.willie.command.Command;
-import com.drtshock.willie.command.DonateCommandHandler;
-import com.drtshock.willie.command.HelpCommandHandler;
-import com.drtshock.willie.command.IssuesCommandHandler;
-import com.drtshock.willie.command.LatestCommandHandler;
-import com.drtshock.willie.command.PluginCommandHandler;
-import com.drtshock.willie.command.PopcornCommandHandler;
-import com.drtshock.willie.command.RepoCommandHandler;
-import com.drtshock.willie.command.RulesCommandHandler;
-import com.drtshock.willie.command.TWSSCommandHandler;
 import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 
@@ -42,11 +32,11 @@ public class Willie extends PircBotX {
 	public CommandManager commandManager;
     private WillieConfig willieConfig;
 
-    public Willie() throws IOException {
+    public Willie() {
         this(new WillieConfig());
     }
 	
-	public Willie(WillieConfig config) throws IOException {
+	public Willie(WillieConfig config) {
         super();
         this.willieConfig = config;
 		
@@ -65,37 +55,84 @@ public class Willie extends PircBotX {
 		this.commandManager.registerCommand(new Command("p", "pop some popcorn!", new PopcornCommandHandler()));
 		this.commandManager.registerCommand(new Command("twss", "that's what she said!", new TWSSCommandHandler()));
 		this.commandManager.registerCommand(new Command("donate", "shows donation info", new DonateCommandHandler()));
+
+        this.commandManager.registerCommand(new Command("join", "<channel> - Joins a channel", new JoinCommandHandler(), true));
+        this.commandManager.registerCommand(new Command("reload", "Reloads willie", new ReloadCommandHandler(), true));
 		
 		this.setName(willieConfig.getNick());
 		this.setVerbose(false);
 		this.getListenerManager().addListener(this.commandManager);
 	}
 
-    public void connect() throws NickAlreadyInUseException, IrcException, IOException {
-        this.connect(willieConfig.getServer());
-        this.setAutoReconnectChannels(true);
-        logger.info("Connected to '" + willieConfig.getServer() + "'");
+    public void connect() {
+        try {
+            this.connect(willieConfig.getServer());
+            this.setAutoReconnectChannels(true);
+            logger.info("Connected to '" + willieConfig.getServer() + "'");
 
-        for (String channel : willieConfig.getChannels()){
-            this.joinChannel(channel);
-            logger.info("Joined channel '" + channel + "'");
+            for (String channel : willieConfig.getChannels()){
+                this.joinChannel(channel);
+                logger.info("Joined channel '" + channel + "'");
+            }
+
+            (new Timer()).schedule(new IssueNotifierTask(this), 300000, 300000); // 5 minutes
+        } catch (NickAlreadyInUseException e) {
+            logger.severe("That nickname is already in use!");
+        } catch (IrcException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reload() {
+        logger.info("Reloading...");
+        willieConfig = WillieConfig.loadFromFile(CONFIG_FILE);
+
+        // Update command prefix
+        commandManager.setCommandPrefix(willieConfig.getCommandPrefix());
+
+        // Nick
+        if(!willieConfig.getNick().equals(getNick())) {
+            setNick(willieConfig.getNick());
+            logger.info("Nick updated.");
         }
 
-        (new Timer()).schedule(new IssueNotifierTask(this), 300000, 300000); // 5 minutes
+        // Server
+        if(!willieConfig.getServer().equals(getServer())) {
+            for(Channel channel : getChannels()) {
+                channel.sendMessage("Uh oh...looks like I'm on the wrong server.");
+            }
+            logger.info("Bot seems to be on the wrong server! Reconnecting...");
+            disconnect();
+            connect();
+        }
+
+        // Channels
+        ArrayList<String> newChannels = willieConfig.getChannels();
+        Set<String> oldChannels = getChannelsNames();
+        for(Channel channel : getChannels()) channel.sendMessage(Colors.RED + "Reloading...");
+        for(String channel : willieConfig.getChannels()) {
+            if(!oldChannels.contains(channel)) {
+                joinChannel(channel);
+                logger.info("Joined new channel " + channel);
+                getChannel(channel).sendMessage(Colors.GREEN + "Someone told me I belong here.");
+            }
+        }
+        for(String channel : oldChannels) {
+            if(!newChannels.contains(channel)) {
+                getChannel(channel).sendMessage(Colors.RED + "Looks like I don't belong here...");
+                logger.info("Leaving channel " + channel);
+                partChannel(getChannel(channel));
+            }
+        }
     }
+
     public WillieConfig getConfig() {
         return willieConfig;
     }
 	
 	public static void main(String[] args){
-		try{
-            Willie willie = new Willie(WillieConfig.loadFromFile("config.yml"));
-            willie.connect();
-		} catch (NickAlreadyInUseException ex){
-		    Willie.logger.severe("Nickname already in use!");
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
+        Willie willie = new Willie(WillieConfig.loadFromFile("config.yml"));
+        willie.connect();
 	}
 	
 }
