04b024a69a57d62798a2e3af0af306f6a630c77b
willyrgfwkw0m <jinggangcong1204@163.com>
2021-08-04 14:26:30 +0800

    Adds a command that shows various CI project info.

--- src/main/java/com/drtshock/willie/JenkinsJob.java
+++ src/main/java/com/drtshock/willie/JenkinsJob.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class JenkinsJob {
 	
 	private String displayName;
 	private String url;
 	private String gitHubUrl;
-	
+    private ArrayList<HealthReport> healthReport;
+
+    public static class HealthReport {
+        public String description;
+        public int score;
+    }
+
 	public String getDisplayName(){
 		return this.displayName;
 	}
@@ -19,6 +26,10 @@ public class JenkinsJob {
 	public String getUrl(){
 		return this.url;
 	}
+
+    public ArrayList<HealthReport> getHealthReports() {
+        return healthReport;
+    }
 	
 	public String getGitHubUrl() throws IOException {
 		if (this.gitHubUrl == null){
@@ -66,23 +77,23 @@ public class JenkinsJob {
 		if (gitUrl == null){
 			return new GitHubIssue[0];
 		}
-		
+
 		URL url = new URL("https://api.github.com/repos/" + gitUrl.substring(19) + "issues");
-		
+
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		
+
 		connection.setConnectTimeout(5000);
 		connection.setReadTimeout(5000);
 		connection.setUseCaches(false);
 		//connection.setRequestProperty("Authorization", Willie.GIT_AUTH);
 		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
-                
+
 		BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		
+
 		GitHubIssue[] issues = Willie.gson.fromJson(input, GitHubIssue[].class);
-		
+
 		input.close();
-		
+
 		return issues;
 	}
 	
