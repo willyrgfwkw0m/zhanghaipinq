49bbbf343d67808a070f48141dd7d27b05c22928
willyrgfwkw0m <jinggangcong1204@163.com>
2021-12-11 12:29:21 +0800

    Revert "JSON that"
    This reverts commit a43789add7e657baf80e44ac1807ad4f8d5ac84b.

--- src/main/java/com/drtshock/willie/command/minecraft/ServerCommandHandler.java
+++ src/main/java/com/drtshock/willie/command/minecraft/ServerCommandHandler.java
@@ -2,19 +2,7 @@ package com.drtshock.willie.command.minecraft;
 
 import com.drtshock.willie.Willie;
 import com.drtshock.willie.command.CommandHandler;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.pircbotx.Channel;
-import org.pircbotx.Colors;
 import org.pircbotx.User;
 
 /**
@@ -25,39 +13,6 @@ public class ServerCommandHandler implements CommandHandler {
 
     @Override
     public void handle(Willie bot, Channel channel, User sender, String[] args) {
-        if (args.length == 0) {
-            channel.sendMessage("Usage: !server <IP>");
-        } else {
-            try {
-                JSONObject json = readJsonFromUrl("http://api.iamphoenix.me/get/?server_ip=" + args[0]);
-                String motd = json.getString("motd");
-                String players = json.getString("players");
-                String max = json.getString("players_max");
-                String version = json.getString("version");
-                channel.sendMessage("(" + args[1] + ") " + motd + " - " + version + " - " + players + "/" + max + " players.");
-            } catch (IOException | JSONException ex) {
-                channel.sendMessage(Colors.RED + "Failed to read json from url.");
-                Logger.getLogger(ServerCommandHandler.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
     }
 
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-            is.close();
-            return json;
-        }
-    }
-
-    public static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
 }
