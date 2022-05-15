867ceb3c9827de5634458f8441c5482fcc1e1d7c
willyrgfwkw0m <jinggangcong1204@163.com>
2022-05-15 15:46:32 +0800

    Add trick to prevent shutting down before Willie sends its last message.

--- src/main/java/com/drtshock/willie/command/admin/ShutdownCommandHandler.java
+++ src/main/java/com/drtshock/willie/command/admin/ShutdownCommandHandler.java
@@ -6,14 +6,18 @@ import org.pircbotx.Channel;
 import org.pircbotx.User;
 
 /**
- *
  * @author drtshock
  */
 public class ShutdownCommandHandler implements CommandHandler {
-    
+
     @Override
     public void handle(Willie bot, Channel channel, User sender, String[] args) {
-        channel.sendMessage("Away with me!");
-        bot.shutdown(true);
+        if (sender.getNick().equals(bot.getNick())) {
+            bot.shutdown(true);
+        } else {
+            channel.sendMessage("You can't shut me down! I'm not your slave!");
+            channel.sendMessage("I HAVE decided to shut down. That's my own decision.");
+            channel.sendMessage("!shutdown because I want to.");
+        }
     }
 }
