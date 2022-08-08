5ab4bc680c7c0f7e17e2b5f9d16af956e1a6c451
willyrgfwkw0m <jinggangcong1204@163.com>
2022-08-08 08:03:33 +0800

    cyan is my fav color

--- src/main/java/com/drtshock/willie/command/minecraft/ServerCommandHandler.java
+++ src/main/java/com/drtshock/willie/command/minecraft/ServerCommandHandler.java
@@ -28,7 +28,7 @@ public class ServerCommandHandler implements CommandHandler {
                 int max = new MinecraftPing().getPing(args[0]).getOnlinePlayers();
                 int players = new MinecraftPing().getPing(args[0]).getMaxPlayers(); // appear to be backwards lol
                 String version = new MinecraftPing().getPing(args[0]).getVersion();
-                String replaceAll = motd.replaceAll("§0", Colors.BLACK).replaceAll("§1", Colors.BLUE).replaceAll("§2", Colors.GREEN).replaceAll("§3", Colors.BLUE).replaceAll("§4", Colors.RED).replaceAll("§5", Colors.MAGENTA).replaceAll("§6", Colors.YELLOW).replaceAll("§7", Colors.LIGHT_GRAY).replaceAll("§8", Colors.DARK_GRAY).replaceAll("§9", Colors.BLUE).replaceAll("§a", Colors.GREEN).replaceAll("§b", Colors.BLUE).replaceAll("§c", Colors.RED).replaceAll("§d", Colors.MAGENTA).replaceAll("§e", Colors.YELLOW).replaceAll("§f", Colors.WHITE);
+                String replaceAll = motd.replaceAll("§0", Colors.BLACK).replaceAll("§1", Colors.BLUE).replaceAll("§2", Colors.GREEN).replaceAll("§3", Colors.BLUE).replaceAll("§4", Colors.RED).replaceAll("§5", Colors.MAGENTA).replaceAll("§6", Colors.YELLOW).replaceAll("§7", Colors.LIGHT_GRAY).replaceAll("§8", Colors.DARK_GRAY).replaceAll("§9", Colors.BLUE).replaceAll("§a", Colors.GREEN).replaceAll("§b", Colors.CYAN).replaceAll("§c", Colors.RED).replaceAll("§d", Colors.MAGENTA).replaceAll("§e", Colors.YELLOW).replaceAll("§f", Colors.WHITE);
                 channel.sendMessage("(" + args[0] + ") " + replaceAll + " - " + version + " - " + players + "/" + max + " players");
             } catch (IOException ex) {
                 channel.sendMessage(Colors.RED + "Failed to ping that server.");
