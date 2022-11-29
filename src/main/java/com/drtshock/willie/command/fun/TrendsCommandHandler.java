572be0d8901e6959a9423e409b35113b67bbdada
willyrgfwkw0m <jinggangcong1204@163.com>
2022-11-30 06:13:24 +0800

    Fix trends

--- src/main/java/com/drtshock/willie/command/fun/TrendsCommandHandler.java
+++ src/main/java/com/drtshock/willie/command/fun/TrendsCommandHandler.java
@@ -25,15 +25,17 @@ public class TrendsCommandHandler implements CommandHandler {
             Trends trend = twitter.getPlaceTrends(1);
             int trendsToShow = 5;
 
-            channel.sendMessage(Colors.CYAN + "Top " + trendsToShow + " trends on Twitter right now...");
+            StringBuilder sb = new StringBuilder();
 
-            for (Trend t : trend.getTrends()) {
-                if (trendsToShow > 0) {
-                    channel.sendMessage(t.getName());
+            for(Trend t : trend.getTrends()) {
+                if(trendsToShow > 0) {
+                sb.append(t.getName()).append(" ");
+                    trendsToShow--;
                 }
-
-                trendsToShow = -1;
             }
+
+            channel.sendMessage(Colors.NORMAL + "Top " + trendsToShow + " trends on Twitter right now: " + sb.toString().trim());
+
         } catch (TwitterException e) {
             e.printStackTrace();
         }
